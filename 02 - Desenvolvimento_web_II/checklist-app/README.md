c8f27edc981d3155455dc896a6b23e764c715894
alinnecardoso <dev14cardoso@gmail.com>
2024-04-25 19:42:02 -0300

    instalação do Angular

--- 02 - Desenvolvimento_web_II/checklist-app/README.md
+++ 02 - Desenvolvimento_web_II/checklist-app/README.md
@@ -1,4 +1,4 @@
-# AngularTeste
+# ChecklistApp
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 17.3.5.
 
--- 02 - Desenvolvimento_web_II/checklist-app/src/app/app.component.spec.ts
+++ 02 - Desenvolvimento_web_II/checklist-app/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'angular-teste' title`, () => {
+  it(`should have the 'checklist-app' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-teste');
+    expect(app.title).toEqual('checklist-app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-teste');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, checklist-app');
   });
 });
