dcd478d82c3f20c16d3e8b75d8c2134d5253ea95
alinnecardoso <dev14cardoso@gmail.com>
2024-04-26 09:34:30 -0300

    Update Angular CLI version and remove SSR configuration
    This commit updates the Angular CLI version from 17.3.5 to 17.3.6. It also removes the SSR configuration from the project

--- 02 - Desenvolvimento_web_II/checklist-app/src/app/app.component.ts
+++ 02 - Desenvolvimento_web_II/checklist-app/src/app/app.component.ts
@@ -1,16 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { TesteComponent } from './teste/teste.component'
-import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from "./header/header.component";
-
 
 @Component({
-    selector: 'app-root',
-    standalone: true,
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css',
-    imports: [RouterOutlet, TesteComponent, FooterComponent, HeaderComponent]
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'checklist-app';
