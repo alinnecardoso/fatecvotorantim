0e552f74db01e2892a56dbad22b772ac216846aa
alinnecardoso <dev14cardoso@gmail.com>
2024-04-26 10:14:21 -0300

    Remove FooterComponent from AppComponent imports
    This commit removes the FooterComponent from the imports array in the AppComponent's decorator. The FooterComponent is no longer used or needed in the AppComponent's template.

--- 02 - Desenvolvimento_web_II/checklist-app/src/app/app.component.ts
+++ 02 - Desenvolvimento_web_II/checklist-app/src/app/app.component.ts
@@ -2,14 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { TestComponent } from "./test/test.component";
 import { HeaderComponent } from "./header/header.component";
-import { FooterComponent } from "./footer/footer.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, TestComponent, HeaderComponent, FooterComponent]
+    imports: [RouterOutlet, TestComponent, HeaderComponent]
 })
 export class AppComponent {
   title = 'checklist-app';
