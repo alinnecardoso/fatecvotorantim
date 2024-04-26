3e7e67977e0440c94268bc6c25019e932a57bc23
alinnecardoso <dev14cardoso@gmail.com>
2024-04-25 21:55:26 -0300

    Resolver erro de importação

--- 02 - Desenvolvimento_web_II/checklist-app/src/app/app.component.ts
+++ 02 - Desenvolvimento_web_II/checklist-app/src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TesteComponent } from './teste/teste.component'
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from "./header/header.component";
+
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, TesteComponent, FooterComponent, HeaderComponent]
 })
 export class AppComponent {
   title = 'checklist-app';
