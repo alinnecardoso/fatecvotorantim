2ba27708faf6bf1d34782e5dd02ec94d0c794bc8
alinnecardoso <dev14cardoso@gmail.com>
2024-05-03 21:01:55 -0300

    Aula Estrutura de dados

--- 02 - Semestre/02 - Estrutura_Dados/ED_1S2024/src/Balanceamento/Balanceamento.java
+++ 02 - Semestre/02 - Estrutura_Dados/ED_1S2024/src/Balanceamento/Balanceamento.java
@@ -11,17 +11,39 @@ package Balanceamento;
  */
 public class Balanceamento {
     public static void main(String[] args){
-        String vetor[] = new String[]{"(", "}"};
+        
+        String vetor[] = new String[]{"(", ")"};
         
         System.out.println(validaBalanceamento(vetor));
         
-        String vetor2[] = new String[]{"{", "}"};
+        String vetor2[] = new String[]{"{", "}","]"};
         
         System.out.println(validaBalanceamento(vetor2));
+        
+        
     }
     
     public static boolean validaBalanceamento(String[] vetor){
-        
-        return false;
+        Pilha p = new Pilha();
+        for(int i=0; i<vetor.length; i++){
+            // vetor[i] == simbolo de abertura
+            if(vetor[i] == "(" || vetor[i] == "{" || vetor[i] == "["){
+                if(vetor[i] == "(")
+                    p.empilhar(")");
+                else if("{".equals(vetor[i]))
+                    p.empilhar("}");
+                else if(vetor[i] == "[")
+                    p.empilhar("]");
+            }
+            if(vetor[i] == ")" || vetor[i] == "}" || vetor[i] == "]"){
+                if(p.pilhaVazia())
+                    return false;
+                else if(vetor[i] != p.desempilhar())
+                    return false;
+            }
+        }if(p.pilhaVazia())
+            return true;
+        else
+            return false;
     }
 }
