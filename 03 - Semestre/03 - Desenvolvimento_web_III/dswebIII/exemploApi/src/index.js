ee4050e56f917fcc52eda46805bcc600bd5f83fa
alinnecardoso <alinne14cardoso@gmail.com>
2024-08-13 22:07:01 -0300

    Exercicio 01

--- 03 - Semestre/03 - Desenvolvimento_web_III/dswebIII/exemploApi/src/index.js
+++ 03 - Semestre/03 - Desenvolvimento_web_III/dswebIII/exemploApi/src/index.js
@@ -2,9 +2,52 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.json())
+
+let nomes = [];
+
 app.get('/', (req, res) => {
-    res.send('Hello world!')
+    res.json(nomes);
 })
+
+app.post('/', (req, res) =>{
+    let nome = req.body.name
+    nomes.push(nome)
+    res.send(`Nome: ${req.body.name}`)
+})
+
+app.post('/exercicio', (req, res) => {
+    const {temperatura} = req.body;
+    const {tipo} = (req.body).toUpperCase();
+    const newTemp = converte(tipo, temperatura);
+    res.json(newTemp);
+})
+
+function converte(tipo, temperatura){
+    switch (tipo) {
+        case "C":
+            return resultado = (9*temperatura/5) + 32;
+        case "F":
+            return resultado = 5 *(temperatura - 32)/9;
+        default:
+            return resultado = 0;
+    }
+    
+}
+
+app.post('/exercicio02', (req, res) =>{
+    let {nota} = req.body
+    let {qtdAlunos} = req.body
+    const calcularNota =  calcularNota(nota, qtdAlunos)
+    res.json(calcularNota)
+})
+
+function calcularNota(nota, qtdAlunos){
+    let lista = [];
+    lista.push(nota);
+    
+    console.log(lista)
+}
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
